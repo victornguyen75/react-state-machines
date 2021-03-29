2b86ac402e18fc1da1137ae197e47a7eb6c18ae2
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-03-29 12:48:09 -0500

    useState setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const color = "Green";
+  const [timeLeft, setTimeLeft] = useState(10);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(timeLeft < 1 ? 10 : timeLeft - 1);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Traffic Light: {color}</p>
+      <p>Time left: {timeLeft}</p>
     </div>
   );
 }
